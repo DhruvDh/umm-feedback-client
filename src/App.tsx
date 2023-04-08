d3079ca5ec5aace72592688e4e2e0c78e6dde8f9
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2023-04-08 17:44:55 -0400

    added /prompts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "@solidjs/router";
 import { Component, lazy } from "solid-js";
+import Messages from "./components/Messages";
+import Prompts from "./components/Prompts";
 const Feedback = lazy(() => import("./components/Feedback"));
 
 const App: Component = () => {
   return (
     <Routes>
+      <Route path="/prompts/:id" component={Prompts} />
       <Route path="/:id" component={Feedback} />
     </Routes>
   );
---
+++ src/components/Prompts.tsx
@@ -0,0 +1,123 @@
+import {
+  Accessor,
+  createEffect,
+  createResource,
+  createSignal,
+  For,
+  mergeProps,
+  Show,
+} from "solid-js";
+import { ChatCompletionRequestMessage } from "openai";
+import { micromark } from "micromark";
+import { gfm, gfmHtml } from "micromark-extension-gfm";
+import { createClient } from "@supabase/supabase-js";
+import { useParams } from "@solidjs/router";
+
+interface PromptProps {
+  uuid: string;
+  feedbackDone: Accessor<boolean>;
+}
+
+const supabase = createClient(
+  "https://uyancztmzjlekojeproj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5YW5jenRtempsZWtvamVwcm9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA4NDA1NzgsImV4cCI6MTk3NjQxNjU3OH0.yMvOYM0AM61v6MRsHUSgO0BPrQHTde2AiKzE0b4H4lo"
+);
+
+interface PromptRow {
+  id: string;
+  reqName: string;
+  grade: string;
+  reason: string;
+  messages: ChatCompletionRequestMessage[];
+  length?: number;
+  previousPrompt?: string;
+}
+
+const getQuery = async (uuid: string): Promise<PromptRow> => {
+  const { data, error } = await supabase
+    .from("prompts")
+    .select("*")
+    .eq("id", uuid)
+    .single();
+
+  if (error) throw error;
+
+  let reqName = data.requirement_name ?? "ITSC 2214 Autograder Feedback";
+  let grade = data.grade ?? "Not Found";
+  let reason = data.reason ?? "";
+  if (reason == "See above.") {
+    reason = "See below.";
+  }
+  let messages: ChatCompletionRequestMessage[] = data.messages ?? [];
+  let previousPrompt = data.previousPrompt;
+
+  if (data["previousPrompt"] !== null) {
+    const { data, error } = await supabase
+      .from("feedback")
+      .select("*")
+      .eq("id", previousPrompt)
+      .single();
+
+    messages.push({
+      role: "assistant",
+      content: data.response,
+      name: "AI Teaching Assistant",
+    });
+  } else {
+    console.log("No previous prompt found.");
+  }
+
+  return {
+    id: uuid,
+    reqName,
+    grade,
+    reason,
+    messages,
+  };
+};
+
+export default function Prompts(props: PromptProps) {
+  const params = useParams();
+  const uuid = params["id"];
+  const [messages] = createResource(uuid, getQuery);
+
+  const [markdown, setMarkdown] = createSignal("");
+
+  createEffect(() => {
+    if (messages.loading) {
+      setMarkdown("Loading...");
+    } else if (messages.error) {
+      setMarkdown("Error loading messages.");
+    } else {
+      let result = "";
+      for (const message of messages().messages) {
+        const name = message.name ?? message.role;
+        const text = message.content;
+
+        result += `\n\n### ${name} Message\n\n${text}`;
+      }
+
+      setMarkdown(result);
+    }
+  });
+
+  return (
+    <article class="mx-auto p-4 prose max-w-3xl">
+      <hr />
+
+      <h2>Information shared with AI</h2>
+      <blockquote>
+        The following is a transcript of information shared with the AI, in
+        order for the above feedback to be generated. If certain important
+        information is missing, that might be why the AI's feedback is not as
+        helpful.
+      </blockquote>
+      <div
+        innerHTML={micromark(markdown(), {
+          extensions: [gfm()],
+          htmlExtensions: [gfmHtml()],
+        })}
+      />
+    </article>
+  );
+}
