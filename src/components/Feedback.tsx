982c98d88eb31892843f75329448ceb75297b46b
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2023-09-26 12:34:36 -0400

    using deno deploy instead

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -36,7 +36,7 @@ export default function Feedback() {
       setConnectionMessage("Loading...");
     } else if (getPrompt.error) {
       setConnectionMessage(
-        "Error finding ID in database. Is the URL correct?.",
+        "Error finding ID in database. Is the URL correct?."
       );
     } else {
     }
@@ -59,120 +59,110 @@ export default function Feedback() {
       setFeedbackDone(true);
       setFoundInDB(true);
     } catch (e) {
-      await fetchEventSource(
-        "https://uyancztmzjlekojeproj.functions.supabase.co/openai-feedback",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "text/event-stream",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5YW5jenRtempsZWtvamVwcm9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA4NDA1NzgsImV4cCI6MTk3NjQxNjU3OH0.yMvOYM0AM61v6MRsHUSgO0BPrQHTde2AiKzE0b4H4lo",
-          },
-          body: JSON.stringify({
-            prompt_id: uuid,
-          }),
-          signal: ctrl.signal,
-          openWhenHidden: true,
-          async onopen(response) {
-            setConnectionOpened(true);
-            if (
-              response.ok &&
-              response.headers.get("content-type") == "text/event-stream"
-            ) {
-              setConnectionMessage("Connected to OpenAI, loading...");
-              return; // everything's good
-            } else if (
-              response.status >= 400 &&
-              response.status < 500 &&
-              response.status !== 429
-            ) {
-              let m = `Fatal error (status ${response.status}): ` +
-                JSON.stringify(response) +
-                "\nIt is likely the conversation is too long or malformed.";
-              setConnectionMessage(m);
-              ctrl.abort(m);
-              // client-side errors are usually non-retriable:
-              throw new FatalError();
-            } else {
-              setConnectionMessage(
-                "Retriable error (something has gone wrong that is not fatal, you should be able to retry in a few minutes.): " +
-                  response.statusText,
-              );
+      await fetchEventSource(`https://umm-feedback-openai.deno.dev/${uuid}`, {
+        method: "GET",
+        signal: ctrl.signal,
+        openWhenHidden: true,
+        async onopen(response) {
+          setConnectionOpened(true);
+          if (
+            response.ok &&
+            response.headers.get("content-type") == "text/event-stream"
+          ) {
+            setConnectionMessage("Connected to OpenAI, loading...");
+            return; // everything's good
+          } else if (
+            response.status >= 400 &&
+            response.status < 500 &&
+            response.status !== 429
+          ) {
+            let m =
+              `Fatal error (status ${response.status}): ` +
+              JSON.stringify(response) +
+              "\nIt is likely the conversation is too long or malformed.";
+            setConnectionMessage(m);
+            ctrl.abort(m);
+            // client-side errors are usually non-retriable:
+            throw new FatalError();
+          } else {
+            setConnectionMessage(
+              "Retriable error (something has gone wrong that is not fatal, you should be able to retry in a few minutes.): " +
+                response.statusText
+            );
 
-              throw new RetriableError();
-            }
-          },
-          onmessage(msg) {
-            // if the server emits an error message, throw an exception
-            // so it gets handled by the onerror callback below:
-            if (msg.event === "FatalError") {
-              setConnectionMessage(
-                "Fatal error: " +
-                  JSON.stringify(msg) +
-                  "\nIt is likely the conversation is too long.",
-              );
-              ctrl.abort(
-                "Fatal error: " +
-                  JSON.stringify(msg) +
-                  "\nIt is likely the conversation is too long.",
-              );
-
-              throw new FatalError(msg.data);
-            }
-            const data = msg.data;
-            if (data == "[DONE]") {
-              return;
-            } else {
-              const val = JSON.parse(data);
-              setFeedback(
-                (prev) =>
-                  (prev === undefined ? "" : prev) +
-                  (val.choices[0].delta.content === undefined
-                    ? ""
-                    : val.choices[0].delta.content),
-              );
-              if (val.choices[0].finish_reason !== null) {
-                setFeedbackDone(true);
-                supabase
-                  .from("feedback")
-                  .insert({ id: uuid, response: feedback() })
-                  .then((res) => {
-                    console.log(res);
-                  });
-              }
-            }
-          },
-          onclose() {
-            if (feedbackDone()) {
-              setConnectionMessage("Done!");
-              setFeedbackDone(true);
-              ctrl.abort("Done!");
-            } else {
-              setConnectionMessage(
-                "Connection closed unexpectedly. (something has gone wrong that is not fatal, you should be able to retry in a few minutes.)",
-              );
-              ctrl.abort(
-                "Connection closed unexpectedly. (something has gone wrong that is not fatal, you should be able to retry in a few minutes.)",
-              );
-
-              // if the server closes the connection unexpectedly, retry:
-              throw new RetriableError();
-            }
-          },
-          onerror(err) {
-            if (err.message.trim() == "") {
-              setConnectionMessage("Done!");
-              setFeedbackDone(true);
-              ctrl.abort("Done!");
-              throw err;
-            } else {
-              setConnectionMessage("Error: " + err.message);
-              ctrl.abort("Error: " + err.message);
-              throw err;
-            }
-          },
+            throw new RetriableError();
+          }
         },
-      );
+        onmessage(msg) {
+          // if the server emits an error message, throw an exception
+          // so it gets handled by the onerror callback below:
+          if (msg.event === "FatalError") {
+            setConnectionMessage(
+              "Fatal error: " +
+                JSON.stringify(msg) +
+                "\nIt is likely the conversation is too long."
+            );
+            ctrl.abort(
+              "Fatal error: " +
+                JSON.stringify(msg) +
+                "\nIt is likely the conversation is too long."
+            );
+
+            throw new FatalError(msg.data);
+          }
+          const data = msg.data;
+          if (data == "[DONE]") {
+            return;
+          } else {
+            const val = JSON.parse(data);
+            setFeedback(
+              (prev) =>
+                (prev === undefined ? "" : prev) +
+                (val.choices[0].delta.content === undefined
+                  ? ""
+                  : val.choices[0].delta.content)
+            );
+            if (val.choices[0].finish_reason !== null) {
+              setFeedbackDone(true);
+              supabase
+                .from("feedback")
+                .insert({ id: uuid, response: feedback() })
+                .then((res) => {
+                  console.log(res);
+                });
+            }
+          }
+        },
+        onclose() {
+          if (feedbackDone()) {
+            setConnectionMessage("Done!");
+            setFeedbackDone(true);
+            ctrl.abort("Done!");
+          } else {
+            setConnectionMessage(
+              "Connection closed unexpectedly. (something has gone wrong that is not fatal, you should be able to retry in a few minutes.)"
+            );
+            ctrl.abort(
+              "Connection closed unexpectedly. (something has gone wrong that is not fatal, you should be able to retry in a few minutes.)"
+            );
+
+            // if the server closes the connection unexpectedly, retry:
+            throw new RetriableError();
+          }
+        },
+        onerror(err) {
+          if (err.message.trim() == "") {
+            setConnectionMessage("Done!");
+            setFeedbackDone(true);
+            ctrl.abort("Done!");
+            throw err;
+          } else {
+            setConnectionMessage("Error: " + err.message);
+            ctrl.abort("Error: " + err.message);
+            throw err;
+          }
+        },
+      });
     }
   });
 
